336fcbf0d6ecec3ac3fb0766a6bbfff37b3d2384
Trevor Bond <trevorkbond@gmail.com>
2024-02-19 12:54:32 -0700

    Add JavaScript for job title button functionality

--- jobs.js
+++ jobs.js
@@ -1,3 +1,24 @@
 function getUsername() {
-    return localStorage.getItem('userName') ?? 'Mystery Player';
+    const userName = localStorage.getItem('userName');
+    return userName !== '' ? userName : "Mystery User";
+}
+
+function addJobButtons() {
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.classList.add('buttons-container');
+    
+    const editButton = document.createElement('button');
+    editButton.classList.add('btn', 'btn-dark', 'btn-sm', 'padding-button-override');
+    editButton.classList.add('btn', 'btn-primary');
+    editButton.textContent = 'Edit';
+
+    const delButton = document.createElement('button');
+    delButton.classList.add('btn', 'btn-dark', 'btn-sm', 'padding-button-override');
+    delButton.classList.add('btn', 'btn-primary');
+    delButton.textContent = 'Delete';
+
+    buttonsDiv.appendChild(editButton);
+    buttonsDiv.appendChild(delButton);
+
+    return buttonsDiv;
 }
