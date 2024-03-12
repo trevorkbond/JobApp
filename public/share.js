74c4016d2625dce6904ac62a120ad18fdfcf9f9e
Trevor Bond <trevorkbond@gmail.com>
2024-03-11 18:14:03 -0600

    Add 3rd party service call to share.js (quote)

--- public/share.js
+++ public/share.js
@@ -15,4 +15,30 @@ function cancelShareJob() {
     localStorage.removeItem('shareMessage');
 }
 
+async function loadQuote() {
+    fetch("https://type.fit/api/quotes")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        const quoteIndex = getRandomInt(15);
+        const quote = data[quoteIndex].text;
+        const author = data[quoteIndex].author;
+        const editedAuthor = author.match(/^(.*?),/);
+        const finalAuthor = editedAuthor[1];
+
+        const quoteEl = document.getElementById('quote');
+        quoteEl.innerHTML = `
+            <p style="margin-top: 1em;"><i>${quote}</i></p>
+            <p style="margin-bottom: 1em;"> - ${finalAuthor}</p>
+        `
+    });
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
 loadShareMessage();
+loadQuote();
