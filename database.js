009b89f5f34e659104d331365f926bbbe977e11a
Trevor Bond <trevorkbond@gmail.com>
2024-03-21 17:44:57 -0600

    Add database.js for simple login storage

---
+++ database.js
@@ -0,0 +1,40 @@
+const { MongoClient } = require('mongodb');
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const config = require('./dbConfig.json');
+
+const userName = config.userName;
+const password = config.password;
+const hostname = config.hostname;
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+const client = new MongoClient(url);
+const userCollection = client.db('authTest').collection('user');
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
+module.exports = {
+    getUser,
+    createUser,
+    getUserByToken,
+};
