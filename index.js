32934e27e7c91cf50cc5fbd3608dbfb037cd44e9
Trevor Bond <trevorkbond@gmail.com>
2024-03-22 18:20:23 -0600

    Add secureApiRouter to require authentication

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const DB = require('./database.js');
+const bcrypt = require('bcrypt');
 const app = express();
 const authCookieName = 'token';
 
@@ -14,60 +15,94 @@ app.use('/api', apiRouter);
 
 apiRouter.post('/auth/create', async (req, res) => {
   if (await DB.getUser(req.body.email)) {
-    res.status(409).send({ msg: 'Existing user' });
+    res.status(409).send({ msg: 'Username already taken' });
   } else {
     const user = await DB.createUser(req.body.email, req.body.password);
 
     // Set the cookie
     setAuthCookie(res, user.token);
 
-    res.send();
+    res.send({
+      id: user._id,
+    });
   }
 });
 
-apiRouter.post('/jobs', (req, res) => {
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Incorrect username or password' });
+});
+
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+secureApiRouter.post('/jobs', (req, res) => {
   addJob(jobs, req.body);
   const filteredJobs = getFilteredJobs(jobs, req.body.user);
   res.send(filteredJobs);
-})
+});
 
-apiRouter.get('/jobs/single/:jobID', (req, res) => {
+secureApiRouter.get('/jobs/single/:jobID', (req, res) => {
   const jobID = parseInt(req.params.jobID);
   foundJob = getJobFromID(jobs, jobID);
   res.send(foundJob);
-})
+});
 
-apiRouter.get('/jobs/:user', (req, res) => {
+secureApiRouter.get('/jobs/:user', (req, res) => {
   const username = req.params.user;
   const filteredJobs = getFilteredJobs(jobs, username);
   res.send(filteredJobs);
-})
+});
 
-apiRouter.put('/jobs', (req, res) => {
+secureApiRouter.put('/jobs', (req, res) => {
   editJob(jobs, req.body);
   const filteredJobs = getFilteredJobs(jobs, req.body.user);
   res.send(filteredJobs);
-})
+});
 
-apiRouter.put('/jobs/:status', (req, res) => {
+secureApiRouter.put('/jobs/:status', (req, res) => {
   editJobStatus(jobs, req.params.status, req.body.jobID);
   const filteredJobs = getFilteredJobs(jobs, req.body.user);
   res.send(filteredJobs);
-})
+});
 
-apiRouter.delete('/jobs', (req, res) => {
+secureApiRouter.delete('/jobs', (req, res) => {
   deleteJob(jobs, req.body);
   const filteredJobs = getFilteredJobs(jobs, req.body.user);
   res.send(filteredJobs);
-})
+});
 
 const port = 4000;
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
-
-let jobs = [];
-let nextJobID = 0;
 
 function addJob(jobs, newJob) {
   newJob.jobID = nextJobID++;
