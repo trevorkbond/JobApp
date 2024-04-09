1309479aa18ba028e727d023c215b7508f31bffb
Trevor Bond <trevorkbond@gmail.com>
2024-04-08 18:11:26 -0600

    Implement authenticated state on login

---
+++ src/login/authenticated.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export function Authenticated(props) {
+    const userName = props.userName;
+    const navigate = useNavigate();
+
+    function logout() {
+        fetch(`/api/auth/logout`, {
+            method: 'delete',
+        })
+            .catch(() => {
+                // Logout failed. Assuming offline
+            })
+            .finally(() => {
+                localStorage.removeItem('userName');
+                props.onLogout();
+            });
+    }
+
+    return (
+        <div className='app'>
+            <div className="signin-upper-padding"></div>
+            <div className="signin-form">
+                <div style={{ width: 'fit-content', border: 'solid thin black', padding: '2em', borderRadius: '2em' }}>
+                    <h2>Welcome back, {userName}</h2>
+                    <p>Continue to view your jobs or logout</p>
+                    <div className="buttons-container">
+                        <button type="button" className="btn btn-dark" style={{marginRight: '.5em'}} onClick={() => navigate('/jobs')}>View jobs</button>
+                        <button type="button" className="btn btn-dark"
+                            onClick={() => logout()}>Logout</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
 import { Unauthenticated } from './unauthenticated';
-// import { Authenticated } from './authenticated';
+import { Authenticated } from './authenticated';
 import { AuthState } from './authState';
 
 export function Login({ userName, authState, onAuthChange }) {
     return (
-        <Unauthenticated
-            userName={userName}
-            onLogin={(loginUserName) => {
-                onAuthChange(loginUserName, AuthState.Authenticated);
-            }}
-        />
+        <div className='app'>
+            {authState === AuthState.Authenticated && (
+                <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+            )}
+            {authState === AuthState.Unauthenticated && (
+                <Unauthenticated
+                    userName={userName}
+                    onLogin={(loginUserName) => {
+                        onAuthChange(loginUserName, AuthState.Authenticated);
+                    }}
+                />
+            )}
+        </div>
     );
 }
